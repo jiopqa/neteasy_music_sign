bba01e65dcf750dc80d0e83ddcb36a52ab4a75a4
jiopqa <57865175+jiopqa@users.noreply.github.com>
2022-11-02 20:49:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -427,6 +427,7 @@ def taskPool():
 程序的入口
 '''
 if __name__ == '__main__':
-    while True:
-        Timer(0, taskPool, ()).start()
-        time.sleep(60*60*24) # 间隔一天
+    taskPool()
+    #while True:
+    #    Timer(0, taskPool, ()).start()
+    #    time.sleep(60*60*24) # 间隔一天
